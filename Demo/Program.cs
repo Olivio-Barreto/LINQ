76c23a0f864ad8abadf0a0b5ac78f68a439234f8
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-05-15 16:10:22 -0300

    V1

---
+++ Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Demo.Entities;
+using System.Linq;
+
+namespace Demo;
+
+internal class Program
+{
+    static void Main()
+    {
+        var c1 = new Category { Id = 2, Name = "Comidas", Tier = 1 };
+        var c2 = new Category { Id = 1, Name = "Tools", Tier = 1 };
+        var c3 = new Category { Id = 3, Name = "Bebidas", Tier = 3 };
+
+        List<Product> products = new()
+        {
+            new Product
+            {
+                Id = 1, Name = "TV", Price = 2000.00, Category = c2
+            },
+            new Product
+            {
+                Id = 2, Name = "Vinho", Price = 200.00, Category = c3
+            },
+            new Product
+            {
+                Id = 3, Name = "Coxinha", Price = 5.00, Category = c1
+            }
+        };
+
+        var templateList = products.Where(product => product.Price > 100).ToList();
+
+        foreach (var product in templateList)
+        {
+            Console.WriteLine(product);
+        }
+    }
+}
